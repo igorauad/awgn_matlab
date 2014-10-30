c2c3b10529f3ce491f6cb62d9c482d741379b80b
Igor Freire <igor.auad@gmail.com>
2014-10-30 18:01:11 -0300

    Add more comments and different FFT conventions

--- awgn_psd.m
+++ awgn_psd.m
@@ -4,10 +4,16 @@
 clear all
 clc
 
+FFT_convention = 0; % 0 - Classic FFT convention (default on Matlab)
+                    % 1 - Unitary FFT convention
+                    
+% NOTE: the unitary FFT convention is the one in which the FFT 
+% transform is energy-preserving.
+
 %% Define target PSD (noise floor)
 
 % Define noise floor:
-noiseFloor = - 135; % dBm/Hz
+noiseFloor = -135; % dBm/Hz
 
 % Convert to db/Hz:
 noiseFloor_dbHz = noiseFloor - 30;
@@ -42,16 +48,38 @@ noise = randn(nSymbols*N,1)*sqrt(variance);
 noise_splitted = reshape(noise, N, nSymbols);
 
 % FFT
-noise_splitted_fft = fft(noise_splitted, N);
+switch FFT_convention
+    case 0
+        noise_fft = fft(noise_splitted, N);
+    case 1
+        noise_fft = fft(noise_splitted, N) / sqrt(N);
+end
 
-% Simplest Periodogram
-Sk = mean( abs(noise_splitted_fft).^2, 2)/N; % Watts/(fft subcarrier)
+% Simplest Periodogram - in Watts/(FFT subcarrier)
+switch FFT_convention
+    case 0
+        Sk = mean( abs(noise_fft).^2, 2) / N;
+    case 1
+        % Note: for an unitary FFT convention, the mean-square of the noise
+        % time domain samples and the mean square of the FFT points are
+        % equal.
+        % You can check by comparing the following (commmented) lines:
+        % mean(abs(noise_splitted(:,1)).^2)
+        % mean(abs(noise_fft(:,1)).^2)
+        Sk = mean( abs(noise_fft).^2, 2); 
+end
 
 % Define sampling frequency on Nyquist Rate
 fs = 2*bandwidth; % sampling frequency
 
-% Convert periodogram to Watts/Hz:
+% Convert PSD estimator to Watts/Hz:
 Sk_wattsHz = Sk / fs; 
+% Note: when the PSD in Watts/tone `Sk` is adopted, the total power is 
+% computed by sum(Sk * (1/N)), where (1/N) is the digital frequency 
+% spacing. In contrast, if the PSD in Watts/Hz `Sk_wattsHz` is used, the
+% total power becomes sum(Sk_wattsHz * toneSpacing), where toneSpacing is
+% the analog frequency spacing, which is equal to fs/N. Hence, it follows 
+% from this that the factor 1/fs must appear in the PSD.
 
 % Convert to db/Hz:
 Sk_dbHz = 10*log10(Sk_wattsHz);
@@ -69,7 +97,7 @@ xlabel('Analog Frequency (Mhz)')
 ylabel(' Noise PSD (dbm/Hz)')
 grid on
 
-% Alternative (more accurate) periodogram using Welch's method:
+% Alternative (more accurate) PSD estimation using Welch's method:
 figure
 pwelch(noise,[],[],[],fs,'twosided');
 
